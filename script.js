fff8a0778e552a6545aa9df395bba360bfbedf98
Edson Famanda <edson@equipmoz.org>
2021-02-09 16:33:43 +0200

    adicionado movimento de cobra com clique de botoes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // variaveis 
-let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o game
 let context = canvas.getContext("2d"); // renderiza o desenho de canvas
 let box = 32;
 let direction = "right";  // variavel definida para orientar os movimentos da cobra
@@ -10,23 +10,34 @@ snake[0] ={
 }
 
 
-function criarBG(){ // funcao que cria o Background
+function createBG(){ // funcao que cria o Background
     context.fillStyle = "yellow"; 
     context.fillRect(0, 0, 16*box, 16*box); //desenha o nosso rectângulo usando x e y ; altura e largura iniciadas
 }
 
-function criarCobrinha (){ // funcao que desenha a cobra em uma iteracao
+function createSnake (){ // funcao que desenha a cobra em uma iteracao
     for(i = 0; i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function iniciarJogo(){
+// eventos de clique do botao
+document.addEventListener('keydown', update);
+
+// funcao que movimenta a cobra 
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function startGame(){
 
     // chamada de funcoes
-    criarBG();
-    criarCobrinha();
+    createBG();
+    createSnake();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -48,6 +59,6 @@ function iniciarJogo(){
 
 }
 
-let jogo = setInterval(iniciarJogo, 100); // passando intervalo de 100ms para renovar
+let game = setInterval(startGame, 120); // passando intervalo de 100ms para renovar
 
 
