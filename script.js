24e2cb0ef8b1df31a29b73555d43e48045a506d5
Edson Famanda <edson@equipmoz.org>
2021-02-10 16:49:06 +0200

    Finalizando o jogo, quando a cobra tocar alguma parte do seu corpo

--- script.js
+++ script.js
@@ -83,6 +83,13 @@ function snakeMoves(){
 
 function startGame(){
 
+    //
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);  // finalizando o jogo quando cumprir condicao
+            alert('Game over :(');
+        }
+    }
     // chamada de funcoes
     borderless();
     createBG();
